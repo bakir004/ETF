075cace9b9716ac357ce0c071545f49f6da0a4de
bakir004 <bakir.cinjarevic@gmail.com>
2024-05-06 00:45:58 +0200

    list seg fault??

--- zadace/zadaca3/4.cpp
+++ zadace/zadaca3/4.cpp
@@ -1,18 +1,19 @@
-#include <list>
 #include <iostream>
+#include <list>
 #include <vector>
 
 std::vector<int> Razbrajanje(int N, int M) {
     std::vector<int> redoslijed;
     std::list<int> lista;
-    for(int i = 1; i <= N; i++) lista.push_back(i);
+    for (int i = 1; i <= N; i++)
+        lista.push_back(i);
     auto it = lista.begin();
-    while(!lista.empty()) {
+    while (!lista.empty()) {
         redoslijed.push_back(*it);
         lista.erase(it);
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             it++;
-            if(it == lista.end())
+            if (it == lista.end())
                 it = lista.begin();
         }
     }
@@ -20,19 +21,21 @@ std::vector<int> Razbrajanje(int N, int M) {
 }
 
 int OdabirKoraka(int N, int K) {
-    for(int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N; i++) {
         std::vector<int> redoslijed = Razbrajanje(N, i);
-        if(redoslijed[N-1] == K)
+        if (redoslijed[N - 1] == K)
             return i;
     }
     return 0;
 }
 
 int main() {
-    int N = 10, K = 2;
+    int N = 10, K = 3;
+    std::cout << "Unesite broj distrikta u gradu: ";
+    std::cin >> N;
+    std::cout << "Unesite redni broj distrikta u kojem se nalazi restoran: ";
+    std::cin >> K;
     int M = OdabirKoraka(N, K);
-    std::cout << "M: " << M << "\n";
-    for(auto e : Razbrajanje(N, M))
-        std::cout << e << " ";
+    std::cout << "Trazeni korak: " << M;
     return 0;
 }
