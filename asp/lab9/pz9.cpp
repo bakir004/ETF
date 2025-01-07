c6b20ec52ac007c8b1d27213986416471ae7b693
bakir004 <bakir.cinjarevic@gmail.com>
2025-01-07 14:04:20 +0100

    asd glupsot

--- asp/lab9/pz9.cpp
+++ asp/lab9/pz9.cpp
@@ -129,10 +129,42 @@ class HashMapa : public Mapa<K, V> {
         return hashValue;
     }
     Par* DEL;
-public:
-    HashMapa(): velicina(0), DEL(new Par({K(),V()})), kapacitet(POCETNA_VELICINA_HASH_TABELE), niz(new Par*[kapacitet]{}) {
+    std::pair<int, bool> trazi(int pocetnaAdresa, const K& kljuc) {
+        int trenutnaAdresa = pocetnaAdresa;
+        int iteracije = 0;
+        while(niz[trenutnaAdresa] == DEL || niz[trenutnaAdresa]->first != kljuc) {
+            std::cout << "Velicina: " << velicina << "\n";
+            if(niz[trenutnaAdresa] == DEL && velicina == kapacitet)
+                return {trenutnaAdresa, false};
+            std::cout << "Kolizija, " << kljuc << " hoce na adresu " << trenutnaAdresa << " a tamo je " << niz[trenutnaAdresa]->first << '\n';
+            trenutnaAdresa = (trenutnaAdresa + 1) % kapacitet;
+            if(niz[trenutnaAdresa] == nullptr) {
+                return {trenutnaAdresa, false};
+            }
+            if(++iteracije == kapacitet) {
+                /*std::cout << "MJESTA: " << trenutnaAdresa << "\n";*/
+                return {trenutnaAdresa, false};
+            }
+        }
+        return {trenutnaAdresa, true};
     }
+    void dodajNaAdresu(const K& kljuc, int adresa) {
+        /*std::cout << "Kreiran " << kljuc << " na adresi " << adresa << "\n";*/
+        if(niz[adresa] != DEL && niz[adresa] != nullptr) {
+            std::cout << "OVERWRITE: " << kljuc << " uzima mjesto " << niz[adresa]->first << "\n";
+            delete niz[adresa];
+        }
+        velicina++;
+        niz[adresa] = new Par({kljuc, V()});
+    }
+public:
+    HashMapa(): velicina(0), DEL(new Par({K(),V()})), kapacitet(POCETNA_VELICINA_HASH_TABELE), niz(new Par*[kapacitet]{}) { }
     ~HashMapa() {
+        delete DEL;
+        for(int i = 0; i < kapacitet; i++)
+            if(niz[i] != DEL)
+                delete niz[i];
+        delete[] niz;
     }
     HashMapa(const HashMapa& nm) : niz(new Par*[nm.kapacitet]{}), velicina(nm.velicina), kapacitet(nm.kapacitet) {
     }
@@ -144,35 +176,27 @@ public:
     V& operator[](const K& kljuc) {
         unsigned int hashirano = this->hash(kljuc, kapacitet);
         if(niz[hashirano] == nullptr) {
-            std::cout << "Kreiran " << kljuc << " na adresi " << hashirano << "\n";
-            Par* novi = new Par({kljuc, V()});
-            niz[hashirano] = novi;
+            dodajNaAdresu(kljuc, hashirano);
             return niz[hashirano]->second;
         }
-
-        int iteracije = 0;
-        while(niz[hashirano] == DEL || niz[hashirano]->first != kljuc) {
-            std::cout << "Kolizija, " << kljuc << " hoce na adresu " << hashirano << " a tamo je " << niz[hashirano]->first << '\n';
-            hashirano = (hashirano + 1) % kapacitet;
-            if(niz[hashirano] == nullptr) {
-                std::cout << "Kreiran " << kljuc << " na adresi " << hashirano << "\n";
-                Par* novi = new Par({kljuc, V()});
-                niz[hashirano] = novi;
-                return niz[hashirano]->second;
-            }
-            if(++iteracije == kapacitet) {
-                std::cout << "MJESTA: " << hashirano << "\n";
-                return niz[hashirano]->second;
-            }
-        }
-        return niz[hashirano]->second;
+        std::pair<int,bool> pronadjenaAdresa = trazi(hashirano, kljuc);
+        std::cout << "Pronadjena adresa: " << pronadjenaAdresa.first << "\n";
+        if(!pronadjenaAdresa.second)
+            dodajNaAdresu(kljuc, pronadjenaAdresa.first);
+        return niz[pronadjenaAdresa.first]->second;
     }
     void obrisi() {
     }
     void obrisi(const K& kljuc) {
         unsigned int hashirano = this->hash(kljuc, kapacitet);
-        std::cout << "Brisem " << kljuc << " sa adrese " << hashirano << '\n';
-        niz[hashirano] = DEL;
+        std::pair<int,bool> pronadjenaAdresa = trazi(hashirano, kljuc);
+        if(!pronadjenaAdresa.second) {
+            std::cout << "Kljuc ne postoji u mapi\n";
+            return;
+        }
+        std::cout << "Brisem " << kljuc << " sa adrese " << pronadjenaAdresa.first << '\n';
+        delete niz[pronadjenaAdresa.first];
+        niz[pronadjenaAdresa.first] = DEL;
     }
     int brojElemenata() const { return velicina; }
     void ispisi() const {
@@ -204,6 +228,16 @@ int main() {
     mapa["g"] = 7;
     mapa["gewadoloid"] = 7;
     mapa["gewaoloid"] = 7;
+    mapa.ispisi();
+    std::cout << mapa["baki"] << '\n';
+    std::cout << mapa["bakir"] << '\n';
+    mapa.obrisi("bakir");
+    mapa["bakir"] = 77;
+    std::cout << mapa["bakir"] << '\n';
+    mapa.ispisi();
+
+    std::cout << "Dalje\n";
+    mapa["divan123"] = 6969;
     mapa.ispisi();
 
     return 0;
